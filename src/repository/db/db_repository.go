5faa4212ebf12376b3d9da71107a3643be212650
yikezawa <y.ikezawa93@gmail.com>
2019-12-17 22:37:10 +0900

    modified

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -4,6 +4,11 @@ import (
 	"github.com/IkezawaYuki/videostore_oauth-api/src/clients/cassandra"
 	"github.com/IkezawaYuki/videostore_oauth-api/src/domain/access_token"
 	"github.com/IkezawaYuki/videostore_users-api/utils/errors"
+	"github.com/gocql/gocql"
+)
+
+const(
+	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
 )
 
 func NewRepository()DbRepository{
@@ -24,6 +29,18 @@ func (r *dbRepository) GetByID(id string)(*access_token.AccessToken, *errors.Res
 	}
 	defer session.Close()
 
-	return nil, errors.NewInternalServerErr("database connection not implemented yet")
+	var result access_token.AccessToken
+	if err := session.Query(queryGetAccessToken, id).Scan(
+		&result.AccessToken,
+		&result.UserID,
+		&result.ClientID,
+		&result.Expires,
+		); err != nil{
+			if err == gocql.ErrNotFound{
+				return nil, errors.NewNotFoundErr("no access token found with given id")
+			}
+		return nil, errors.NewInternalServerErr(err.Error())
+	}
+	return &result, nil
 }
 

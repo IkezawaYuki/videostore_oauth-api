b3fc48d8d1d1bc14e2fe2c9bbe19f6dbb8753368
yikezawa <y.ikezawa93@gmail.com>
2019-12-27 23:12:14 +0900

    test

--- src/repository/rest/users_repository_test.go
+++ src/repository/rest/users_repository_test.go
@@ -1,1 +1,28 @@
 package rest
+
+import (
+	"github.com/mercadolibre/golang-restclient/rest"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	rest.StartMockupServer()
+	os.Exit(m.Run())
+}
+
+func TestLoginUserTimeoutFromApi(t *testing.T) {
+
+}
+func TestLoginUserInvalidErrorInterface(t *testing.T) {
+
+}
+func TestLoginUserInvalidLoginCredentials(t *testing.T) {
+
+}
+func TestLoginUserInvalidUserJsonResponse(t *testing.T) {
+
+}
+func TestLoginUserNoError(t *testing.T) {
+
+}
